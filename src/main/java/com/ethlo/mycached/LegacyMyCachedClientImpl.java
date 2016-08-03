a1117d43fb308cbe2739f710368a477c492111b8
Morten Haraldsen <m.haraldsen@kezzler.com>
2016-08-03 11:47:51 +0200

    Add support for iterating key/values

--- src/main/java/com/ethlo/mycached/LegacyMyCachedClientImpl.java
+++ src/main/java/com/ethlo/mycached/LegacyMyCachedClientImpl.java
@@ -5,13 +5,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map.Entry;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,9 +26,13 @@ import org.springframework.util.Assert;
 
 import com.ethlo.keyvalue.BatchCasKeyValueDb;
 import com.ethlo.keyvalue.CasHolder;
+import com.ethlo.keyvalue.IterableKeyValueDb;
 import com.ethlo.keyvalue.MutatingKeyValueDb;
+import com.ethlo.keyvalue.SeekableIterator;
 import com.ethlo.keyvalue.keys.ByteArrayKey;
 import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import com.google.common.collect.AbstractIterator;
 
 /**
  * Works by using standard SQL for handling data operations instead of the MySql-MemCached interface
@@ -32,6 +41,7 @@ import com.google.common.base.Function;
  */
 public class LegacyMyCachedClientImpl implements 
 	MutatingKeyValueDb<ByteArrayKey, byte[]>,
+	IterableKeyValueDb<ByteArrayKey, byte[]>,
 	BatchCasKeyValueDb<ByteArrayKey, byte[], Long>
 {
 	private JdbcTemplate tpl;
@@ -40,13 +50,14 @@ public class LegacyMyCachedClientImpl implements
 	
 	private final String getSql;
 	private final String getCasSql;
+	private final String getCasSqlPrefix;
 	private final String insertSql;
 	private final String replaceSql;
 	private final String replaceCasSql;
 	private final String deleteSql;
 	private final String clearSql;
-	private RowMapper<byte[]> rowMapper;
-	private RowMapper<CasHolder<ByteArrayKey, byte[], Long>> casRowMapper;
+	private final RowMapper<byte[]> rowMapper;
+	private final RowMapper<CasHolder<ByteArrayKey, byte[], Long>> casRowMapper;
 	
 	public LegacyMyCachedClientImpl(String tableName, DataSource dataSource, boolean useCompression)
 	{
@@ -58,6 +69,7 @@ public class LegacyMyCachedClientImpl implements
 		
 		this.getSql = "SELECT mvalue FROM " + tableName + " WHERE mkey = ?";
 		this.getCasSql = "SELECT mkey, mvalue, cas_column FROM " + tableName + " WHERE mkey = ?";
+		this.getCasSqlPrefix = "SELECT mkey, mvalue, cas_column FROM " + tableName + " WHERE mkey LIKE ?";
 		this.replaceSql = "REPLACE INTO " + tableName + " (mkey, mvalue, cas_column) VALUES(?, ?, COALESCE(0, cas_column + 1))";
 		this.insertSql = "INSERT INTO " + tableName + " (mkey, mvalue, cas_column) VALUES(?, ?, ?)";
 		this.replaceCasSql = "UPDATE " + tableName + " SET mvalue = ?, cas_column = cas_column + 1 WHERE mkey = ? AND cas_column = ?";
@@ -79,12 +91,12 @@ public class LegacyMyCachedClientImpl implements
 			@Override
 			public CasHolder<ByteArrayKey, byte[], Long> mapRow(ResultSet rs, int rowNum) throws SQLException
 			{
-				final ByteArrayKey key = new ByteArrayKey(Base64.decodeBase64(rs.getString(1)));
+				final ByteArrayKey key = toKey(rs.getString(1));
 				final byte[] uncompressed = rs.getBytes(2);
 				final byte[] value = useCompression ? CompressionUtil.uncompress(uncompressed) : uncompressed;
 				final long cas = rs.getLong(3);
 				return new CasHolder<ByteArrayKey, byte[], Long>(cas, key, value);
-			}			
+			}
 		};
 	}
 	
@@ -97,7 +109,7 @@ public class LegacyMyCachedClientImpl implements
 		    public PreparedStatement createPreparedStatement(Connection con) throws SQLException
 		    {
 		        final PreparedStatement ps = con.prepareStatement(getSql);
-		        final String strKey = getKey(key);
+		        final String strKey = fromKey(key);
 		        ps.setString(1, strKey);  
 		        return ps;
 		    }
@@ -105,9 +117,23 @@ public class LegacyMyCachedClientImpl implements
 		return DataAccessUtils.singleResult(tpl.query(getPsc, rowMapper));
 	}
 
-	private String getKey(ByteArrayKey key)
+    private ByteArrayKey toKey(String string)
+    {
+        try
+        {
+            return new ByteArrayKey(Hex.decodeHex(string.toCharArray()));
+        }
+        catch (DecoderException exc)
+        {
+            throw Throwables.propagate(exc);
+        }
+        //return new ByteArrayKey(Base64.decodeBase64(string));
+    }
+	
+	private String fromKey(ByteArrayKey key)
 	{
-		return Base64.encodeBase64String(key.getByteArray());
+	    return Hex.encodeHexString(key.getByteArray());
+	    //return Base64.encodeBase64String(key.getByteArray());
 	}
 	
 	@Override
@@ -119,7 +145,7 @@ public class LegacyMyCachedClientImpl implements
 		    public PreparedStatement createPreparedStatement(Connection con) throws SQLException
 		    {
 		        final PreparedStatement ps = con.prepareStatement(replaceSql);
-		        final String strKey = getKey(key);
+		        final String strKey = fromKey(key);
 		        ps.setString(1, strKey); 
 		        ps.setBytes(2, useCompression ? CompressionUtil.compress(value) : value);
 		        return ps;
@@ -131,7 +157,7 @@ public class LegacyMyCachedClientImpl implements
 	@Override
 	public void delete(ByteArrayKey key)
 	{
-		tpl.update(deleteSql, getKey(key));
+		tpl.update(deleteSql, fromKey(key));
 	}
 
 	@Override
@@ -151,7 +177,7 @@ public class LegacyMyCachedClientImpl implements
 	@Override
 	public void close()
 	{
-		
+	    
 	}
 
 	@Override
@@ -163,7 +189,7 @@ public class LegacyMyCachedClientImpl implements
 		    public PreparedStatement createPreparedStatement(Connection con) throws SQLException
 		    {
 		        final PreparedStatement ps = con.prepareStatement(getCasSql);
-		        final String strKey = getKey(key);
+		        final String strKey = fromKey(key);
 		        ps.setString(1, strKey);  
 		        return ps;
 		    }
@@ -192,7 +218,7 @@ public class LegacyMyCachedClientImpl implements
 		    public PreparedStatement createPreparedStatement(Connection con) throws SQLException
 		    {
 		        final PreparedStatement ps = con.prepareStatement(insertSql);
-		        final String strKey = getKey(cas.getKey());
+		        final String strKey = fromKey(cas.getKey());
 		        ps.setString(1, strKey); 
 		        ps.setBytes(2, useCompression ? CompressionUtil.compress(cas.getValue()) : cas.getValue());
 		        ps.setLong(3, 0L);
@@ -217,7 +243,7 @@ public class LegacyMyCachedClientImpl implements
 		    @Override
 		    public PreparedStatement createPreparedStatement(Connection con) throws SQLException
 		    {
-		    	final String strKey = getKey(cas.getKey());
+		    	final String strKey = fromKey(cas.getKey());
 		    	final long casValue = cas.getCasValue();
 		        final PreparedStatement ps = con.prepareStatement(replaceCasSql);
 		        ps.setBytes(1, useCompression ? CompressionUtil.compress(cas.getValue()) : cas.getValue());
@@ -229,7 +255,7 @@ public class LegacyMyCachedClientImpl implements
 		final int rowsChanged = tpl.update(creator);
 		if (rowsChanged == 0)
 		{
-			throw new OptimisticLockingFailureException("Cannot update data due to concurrent modification. Details: Attempted CAS value=" + cas.getCasValue());
+			throw new OptimisticLockingFailureException("Cannot update data for key " + cas.getKey() + " due to concurrent modification. Details: Attempted CAS value=" + cas.getCasValue());
 		}
 	}
 
@@ -268,4 +294,175 @@ public class LegacyMyCachedClientImpl implements
 	{
 		return this.useCompression;
 	}
+
+    @Override
+    public SeekableIterator<ByteArrayKey, byte[]> iterator()
+    {
+        return new JdbcSeekableIterator();
+    }
+    
+    private class JdbcSeekableIterator extends AbstractIterator<Entry<ByteArrayKey, byte[]>> implements SeekableIterator<ByteArrayKey, byte[]>
+    {
+        private ResultSet rs = null;
+        private Connection connection;
+        private PreparedStatement ps;
+        
+        @Override
+        protected Entry<ByteArrayKey, byte[]> computeNext()
+        {
+            try
+            {
+                if (! rs.isAfterLast())
+                {
+                    final CasHolder<ByteArrayKey, byte[], Long> res = casRowMapper.mapRow(rs, rs.getRow());
+                    rs.next();
+                    return new AbstractMap.SimpleEntry<ByteArrayKey, byte[]>(res.getKey(), res.getValue());
+                }
+                return endOfData();
+            }
+            catch (SQLException exc)
+            {
+                throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+            }
+        }
+
+        @Override
+        public void close()
+        {
+            if (rs != null)
+            {
+                try
+                {
+                    rs.close();
+                }
+                catch (SQLException exc)
+                {
+                    throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+                }
+            }
+            
+            if (ps != null)
+            {
+                try
+                {
+                    ps.close();
+                }
+                catch (SQLException exc)
+                {
+                    throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+                }
+            }
+            
+            if (connection != null)
+            {
+                try
+                {
+                    connection.close();
+                }
+                catch (SQLException exc)
+                {
+                    throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+                }
+            }
+        }
+
+        @Override
+        public boolean hasPrevious()
+        {
+            try
+            {
+                final boolean hasPrevious = rs.previous();
+                if (hasPrevious)
+                {
+                    rs.next();
+                }
+                return hasPrevious;
+            }
+            catch (SQLException exc)
+            {
+                throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+            }
+        }
+
+        @Override
+        public Entry<ByteArrayKey, byte[]> previous()
+        {
+            try
+            {
+                if (rs.previous())
+                {
+                    final CasHolder<ByteArrayKey, byte[], Long> res = casRowMapper.mapRow(rs, rs.getRow());
+                    return new AbstractMap.SimpleEntry<ByteArrayKey, byte[]>(res.getKey(), res.getValue());
+                }
+            }
+            catch (SQLException exc)
+            {
+                throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+            }
+            throw new EmptyResultDataAccessException("No previous result", 1);
+        }
+
+        @Override
+        public void seekTo(ByteArrayKey key)
+        {
+            final PreparedStatementCreator getCasPsc = new PreparedStatementCreator()
+            {
+                @Override
+                public PreparedStatement createPreparedStatement(Connection con) throws SQLException
+                {
+                    final PreparedStatement ps = con.prepareStatement(getCasSqlPrefix, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                    final String strKey = fromKey(key);
+                    ps.setString(1, strKey + "%");  
+                    return ps;
+                }
+            };
+            
+            try
+            {
+                if (this.connection == null)
+                {
+                    this.connection = tpl.getDataSource().getConnection();
+                    this.ps = getCasPsc.createPreparedStatement(connection);
+                }
+                this.rs = ps.executeQuery();
+                if (! this.rs.first())
+                {
+                    throw new EmptyResultDataAccessException(1);
+                }
+            }
+            catch (SQLException exc)
+            {
+                throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+            }
+        }
+
+        @Override
+        public void seekToFirst()
+        {
+            if (rs != null)
+            {
+                try
+                {
+                    rs.first();
+                }
+                catch (SQLException exc)
+                {
+                    throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+                }
+            }
+        }
+
+        @Override
+        public void seekToLast()
+        {
+            try
+            {
+                rs.last();
+            }
+            catch (SQLException exc)
+            {
+                throw new DataAccessResourceFailureException(exc.getMessage(), exc);
+            }
+        }
+    }
 }
--- src/test/java/com/ethlo/mycached/SmokeTest.java
+++ src/test/java/com/ethlo/mycached/SmokeTest.java
@@ -14,79 +14,117 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ethlo.keyvalue.CasHolder;
 import com.ethlo.keyvalue.CasKeyValueDb;
+import com.ethlo.keyvalue.IterableKeyValueDb;
 import com.ethlo.keyvalue.MutatingKeyValueDb;
+import com.ethlo.keyvalue.SeekableIterator;
 import com.ethlo.keyvalue.keys.ByteArrayKey;
 import com.google.common.base.Function;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:/mycached-testcontext.xml"})
-@TestExecutionListeners(listeners={DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class})
+@ContextConfiguration(locations = { "classpath:/mycached-testcontext.xml" })
+@TestExecutionListeners(listeners ={ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class, TransactionalTestExecutionListener.class })
+@Transactional
 public class SmokeTest
 {
-	@Resource
-	private LegacyMyCachedClientManagerImpl clientManager;
-	
-	private CasKeyValueDb<ByteArrayKey, byte[], Long> client;
-	
-	@Before
-	public void setup()
-	{
-		final String dbName = "someTestData";
-		this.client = clientManager.createMainDb(dbName, true);
-	}
-	
-	@Test
-	public void putAndGetCompare() throws SQLException
-	{
-		final ByteArrayKey keyBytes = new ByteArrayKey(new byte[]{0,1,2,3,4,5,6,7});
-		final byte[] valueBytes = "ThisIsTheDataToStoreSoLetsmakeItABitLonger".getBytes(StandardCharsets.UTF_8);
-		client.put(keyBytes, valueBytes);
-		final byte[] retVal = client.get(keyBytes);
-		Assert.assertArrayEquals(valueBytes, retVal);
-	}
-	
-	@Test
-	public void testCas() throws SQLException
-	{
-		final ByteArrayKey keyBytes = new ByteArrayKey(new byte[]{4,5,6,7,9,9});
-		final byte[] valueBytes = "ThisIsTheDataToStoreSoLetsmakeItABitLonger".getBytes(StandardCharsets.UTF_8);
-		final byte[] valueBytesUpdated = "ThisIsTheDataToStoreSoLetsmakeItABitLongerAndEvenUpdated".getBytes(StandardCharsets.UTF_8);
-		
-		client.put(keyBytes, valueBytes);
-		
-		final CasHolder<ByteArrayKey, byte[], Long> res = client.getCas(keyBytes);
-		Assert.assertEquals(Long.valueOf(0L), res.getCasValue());
-		Assert.assertArrayEquals(valueBytes, res.getValue());
-		
-		res.setValue(valueBytesUpdated);
-		client.putCas(res);
-	}
-	
-	@Test
-	public void testMutate() throws SQLException
-	{
-		final ByteArrayKey key = new ByteArrayKey(new byte[]{4,5,6,7,9,9});
-		final byte[] valueBytes = "ThisIsTheDataToStoreSoLetsmakeItABitLonger".getBytes(StandardCharsets.UTF_8);
-		final byte[] valueBytesUpdated = "ThisIsTheDataToStoreSoLetsmakeItABitLongerAndEvenUpdated".getBytes(StandardCharsets.UTF_8);
-		
-		client.put(key, valueBytes);
-		
-		@SuppressWarnings("unchecked")
-		final MutatingKeyValueDb<ByteArrayKey, byte[]> mdb = (MutatingKeyValueDb<ByteArrayKey, byte[]>) client;
-		mdb.mutate(key, new Function<byte[], byte[]>()
-		{
-			@Override
-			public byte[] apply(byte[] input)
-			{
-				return valueBytesUpdated;
-			}
-		});
-		
-		final CasHolder<ByteArrayKey, byte[], Long> res = client.getCas(key);
-		Assert.assertEquals(Long.valueOf(1L), res.getCasValue());
-		Assert.assertArrayEquals(valueBytesUpdated, res.getValue());
-	}
+    @Resource
+    private LegacyMyCachedClientManagerImpl clientManager;
+
+    private CasKeyValueDb<ByteArrayKey, byte[], Long> client;
+
+    @Before
+    public void setup()
+    {
+        final String dbName = "someTestData";
+        this.client = clientManager.createMainDb(dbName, true);
+    }
+
+    @Test
+    public void putAndGetCompare() throws SQLException
+    {
+        final ByteArrayKey keyBytes = new ByteArrayKey(new byte[]
+        { 0, 1, 2, 3, 4, 5, 6, 7 });
+        final byte[] valueBytes = "ThisIsTheDataToStoreSoLetsmakeItABitLonger".getBytes(StandardCharsets.UTF_8);
+        client.put(keyBytes, valueBytes);
+        final byte[] retVal = client.get(keyBytes);
+        Assert.assertArrayEquals(valueBytes, retVal);
+    }
+
+    @Test
+    public void testCas() throws SQLException
+    {
+        final ByteArrayKey keyBytes = new ByteArrayKey(new byte[]{ 4, 5, 6, 7, 9, 9 });
+        final byte[] valueBytes = "ThisIsTheDataToStoreSoLetsmakeItABitLonger".getBytes(StandardCharsets.UTF_8);
+        client.put(keyBytes, valueBytes);
+        
+        final CasHolder<ByteArrayKey, byte[], Long> res = client.getCas(keyBytes);
+        Assert.assertEquals(keyBytes, res.getKey());
+        Assert.assertEquals(Long.valueOf(0L), res.getCasValue());
+        Assert.assertArrayEquals(valueBytes, res.getValue());
+
+        final byte[] valueBytesUpdated = "ThisIsTheDataToStoreSoLetsmakeItABitLongerAndEvenUpdated".getBytes(StandardCharsets.UTF_8);
+        res.setValue(valueBytesUpdated);
+        client.putCas(res);
+    }
+
+    @Test
+    public void testIterate() throws SQLException
+    {
+        final ByteArrayKey keyBytes0 = new ByteArrayKey(new byte[] { 0, 0 });
+        final ByteArrayKey keyBytes1 = new ByteArrayKey(new byte[] { 1, 0 });
+        final ByteArrayKey keyBytes2 = new ByteArrayKey(new byte[] { 1, 1 });
+        final ByteArrayKey keyBytes3 = new ByteArrayKey(new byte[] { 1, 2 });
+        final ByteArrayKey keyBytes4 = new ByteArrayKey(new byte[] { 2, 0 });
+        final byte[] valueBytes = "ThisIsTheDataToStoreSoLetsmakeItABitLonger".getBytes(StandardCharsets.UTF_8);
+
+        client.put(keyBytes0, valueBytes);
+        client.put(keyBytes1, valueBytes);
+        client.put(keyBytes2, valueBytes);
+        client.put(keyBytes3, valueBytes);
+        client.put(keyBytes4, valueBytes);
+
+        IterableKeyValueDb<ByteArrayKey, byte[]> idb = (IterableKeyValueDb<ByteArrayKey, byte[]>) client;
+        int count = 0;
+        try (final SeekableIterator<ByteArrayKey, byte[]> iter = idb.iterator())
+        {
+            final ByteArrayKey prefixKey = new ByteArrayKey(new byte[]{1});
+            iter.seekTo(prefixKey);
+            while (iter.hasNext())
+            {
+                System.out.println(iter.next());
+                count++;
+            }
+        }
+        Assert.assertEquals(3, count);
+    }
+
+    @Test
+    public void testMutate() throws SQLException
+    {
+        final ByteArrayKey key = new ByteArrayKey(new byte[]
+        {1});
+        final byte[] valueBytes = "ThisIsTheDataToStoreSoLetsmakeItABitLonger".getBytes(StandardCharsets.UTF_8);
+        final byte[] valueBytesUpdated = "ThisIsTheDataToStoreSoLetsmakeItABitLongerAndEvenUpdated".getBytes(StandardCharsets.UTF_8);
+
+        client.put(key, valueBytes);
+
+        @SuppressWarnings("unchecked")
+        final MutatingKeyValueDb<ByteArrayKey, byte[]> mdb = (MutatingKeyValueDb<ByteArrayKey, byte[]>) client;
+        mdb.mutate(key, new Function<byte[], byte[]>()
+        {
+            @Override
+            public byte[] apply(byte[] input)
+            {
+                return valueBytesUpdated;
+            }
+        });
+
+        final CasHolder<ByteArrayKey, byte[], Long> res = client.getCas(key);
+        Assert.assertEquals(Long.valueOf(1L), res.getCasValue());
+        Assert.assertArrayEquals(valueBytesUpdated, res.getValue());
+    }
 }
